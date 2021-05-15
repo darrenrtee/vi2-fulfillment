dc43a189a8adc9323b93953f14966250f8d67202
Darren Tee <45459431+darrenrtee@users.noreply.github.com>
2021-05-15 23:25:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {WebhookClient} = require('dialogflow-fulfillment')
+
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+app.post('/dialogflow-fulfillment',(request,response) => {
+    dialogflowFulfillment(request,response)
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+})
+
+const dialogflowFulfillment = (request,response) => {
+    const agent = new WebhookClient({request,response})
+
+    function testFunc(agent){
+        agent.add("Hi from heroku")
+    }
+    
+    let intentMap = new Map();
+    intentMap.set("Test",testFunc)
+    agent.handleRequest(intentMap)
+}
